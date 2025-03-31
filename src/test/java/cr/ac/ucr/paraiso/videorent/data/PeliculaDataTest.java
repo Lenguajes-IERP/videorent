d3ce980c4f93584ed7c40b0bbad3cec673260c6c
unknown <alvamen@gmail.com>
2025-03-31 15:50:50 -0600

    PeliculaDataTest

--- src/test/java/cr/ac/ucr/paraiso/videorent/data/PeliculaDataTest.java
+++ src/test/java/cr/ac/ucr/paraiso/videorent/data/PeliculaDataTest.java
@@ -1,5 +1,7 @@
 package cr.ac.ucr.paraiso.videorent.data;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -7,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -16,7 +19,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
+import cr.ac.ucr.paraiso.videorent.domain.Actor;
+import cr.ac.ucr.paraiso.videorent.domain.Genero;
 import cr.ac.ucr.paraiso.videorent.domain.Pelicula;
 
 @SpringBootTest
@@ -26,6 +33,29 @@ public class PeliculaDataTest {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	@Test
+	@Sql (scripts = "/save_pelicula_con_actores.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	public void save_pelicula_con_actores() {
+		// Assert MEJORADO CON UNA INYECCIÓN DE REGISTROS PARA MEJORAR LA PRUEBA UNITARIA
+		//	IDENT_CURRENT() returns the last identity value generated for a specific table, regardless of the scope.
+		// Retrieve generated genero_id
+        Integer generoId = jdbcTemplate.queryForObject("SELECT IDENT_CURRENT('Genero')", Integer.class);
+
+        // Retrieve generated actor1_id and actor2_id
+        Integer actorId1 = jdbcTemplate.queryForObject("SELECT IDENT_CURRENT('Actor')-1", Integer.class);
+        Integer actorId2 = jdbcTemplate.queryForObject("SELECT IDENT_CURRENT('Actor')", Integer.class);
+
+		List<Actor> actores = new LinkedList<>();
+		actores.add(new Actor(actorId1, null, null));
+		actores.add(new Actor(actorId2, null, null));
+		Pelicula pelicula = new Pelicula(0, "The Matrix", true, true, new Genero(generoId, null), actores);
+		
+		//Act
+		assertDoesNotThrow(() -> peliculaData.save(pelicula));
+		// Assert
+		assertNotEquals(0, pelicula.getPeliculaId());
+	}
 
 	@Test
 	public void findMoviesByTitleAndGenre_conTituloGenero_existentes() {
